809ac35ec7d64165ddbeddd3b187b0aebb1f9cd0
Jemadean Dobrosielski <jemadean@gmail.com>
2018-07-23 09:20:59 -0700

    initial namespacing

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,5 +1,19 @@
 'use strict';
 
-const Item = (function () {
+const Item = (function() {
   return {};
 }());
+
+const validateName = (function(name) {
+  if ('Item.name' in Item === false) {
+    console.error('Name does not exist');
+  }
+});
+
+const create = function(name) {
+  id: cuid
+  name: 'name',
+  checked: false
+};
+
+
