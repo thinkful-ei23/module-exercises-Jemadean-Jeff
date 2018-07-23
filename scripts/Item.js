3ab104a161150df50f3530c92e02cd44adec4008
maxjeffwell <maxjeffwell@gmail.com>
2018-07-23 16:19:06 -0400

    removed from global scope

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,24 +1,27 @@
 'use strict';
 
+/* global , cuid */
+
 const Item = (function() {
+  function validateName(name) {
+    if (name === '') {
+      throw new TypeError('Name does not exist');
+    }
+  }
+  
+  function create(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false,
+    };
+  }
   return {
     validateName,
     create,
   };
 }());
 
-function validateName(name) {
-  if (name === '') {
-    throw new TypeError('Name does not exist');
-  }
-}
 
-function create(name) {
-  return {
-    id: cuid(),
-    name: name,
-    checked: false,
-  };
-}
 
 
