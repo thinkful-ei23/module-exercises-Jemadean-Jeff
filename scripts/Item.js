1174b07a0fe0175312f0e8ea934a7b9485090a19
maxjeffwell <maxjeffwell@gmail.com>
2018-07-23 15:17:56 -0400

    myfiles

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,19 +1,24 @@
 'use strict';
-
-const Item = (function() {
-  return {};
-}());
-
 const validateName = (function(name) {
   if ('Item.name' in Item === false) {
     console.error('Name does not exist');
   }
 });
 
-const create = function(name) {
-  id: cuid(),
-  name: 'name',
-  checked: false
-};
+
+
+const Item = (function() {
+  return {};
+}(validateName(name), create(name)));
+
+
+
+const create = (function(name) {
+  return {
+    id: cuid(),
+    name: 'name',
+    checked: false
+  };
+});
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,15 @@
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
   shoppingList.render();
 });
 
