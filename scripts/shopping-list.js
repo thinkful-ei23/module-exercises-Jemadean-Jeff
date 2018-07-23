2bb98a8a6d9c6e19a754bfe10345ecc4c4d691e7
maxjeffwell <maxjeffwell@gmail.com>
2018-07-23 17:06:23 -0400

    added functions to additemtoshoppinglist

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* global store, cuid */
 
 // eslint-disable-next-line no-unused-vars
@@ -56,7 +58,14 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName); 
+      store.items.push(Item.create(itemName));
+    }
+    catch(error) {
+      console.error('Cannot add item: {error.message}');
+    }
+    render();
   }
   
   function handleNewItemSubmit() {
